4f5ae2ee1feb990e01afa5957d67a611525b5455
handr95 <handr95@naver.com>
2024-03-09 22:25:10 +0900

    # 알고리즘 - WEEK6  - 테스트 코드 수정.

--- leetcode/src/test/java/com/study/leetcode/linkedlist/AddTwoNumbersTest.java
+++ leetcode/src/test/java/com/study/leetcode/linkedlist/AddTwoNumbersTest.java
@@ -3,6 +3,7 @@ package com.study.leetcode.linkedlist;
 import com.study.leetcode.common.ListNode;
 import org.junit.jupiter.api.Test;
 
+import static com.study.leetcode.common.ListNode.createLinkedList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AddTwoNumbersTest {
@@ -10,58 +11,26 @@ class AddTwoNumbersTest {
 
     @Test
     void test1() {
-        ListNode l1n1 = new ListNode(3);
-        ListNode l1n2 = new ListNode(4, l1n1);
-        ListNode l1n3 = new ListNode(2, l1n2);
+        ListNode node1 = createLinkedList(new int[]{2, 4, 3});
+        ListNode node2 = createLinkedList(new int[]{6, 6, 4});
+        ListNode expect = createLinkedList(new int[]{8, 0, 7});
 
-        ListNode l2n1 = new ListNode(4);
-        ListNode l2n2 = new ListNode(6, l2n1);
-        ListNode l2n3 = new ListNode(5, l2n2);
-
-        ListNode output = addTwoNumbers.addTwoNumbers(l1n3, l2n3);
-        int[] expect = {7, 0, 8};
-        for (int expectVal : expect) {
-            assertThat(output).isNotNull();
-            assertThat(output.val).isEqualTo(expectVal);
-            output = output.next;
-        }
+        assertThat(addTwoNumbers.addTwoNumbers(node1, node2)).isEqualTo(expect);
     }
 
     @Test
     void test2() {
-        ListNode l1n1 = new ListNode(0);
-        ListNode l2n1 = new ListNode(0);
-
-        ListNode output = addTwoNumbers.addTwoNumbers(l1n1, l2n1);
-        int[] expect = {0};
-        for (int expectVal : expect) {
-            assertThat(output).isNotNull();
-            assertThat(output.val).isEqualTo(expectVal);
-            output = output.next;
-        }
+        ListNode node1 = createLinkedList(new int[]{0});
+        ListNode node2 = createLinkedList(new int[]{0});
+        ListNode expect = createLinkedList(new int[]{0});
+        assertThat(addTwoNumbers.addTwoNumbers(node1, node2)).isEqualTo(expect);
     }
 
     @Test
     void test3() {
-        ListNode l1n1 = new ListNode(9);
-        ListNode l1n2 = new ListNode(9, l1n1);
-        ListNode l1n3 = new ListNode(9, l1n2);
-        ListNode l1n4 = new ListNode(9, l1n3);
-        ListNode l1n5 = new ListNode(9, l1n4);
-        ListNode l1n6 = new ListNode(9, l1n5);
-        ListNode l1n7 = new ListNode(9, l1n6);
-
-        ListNode l2n1 = new ListNode(9);
-        ListNode l2n2 = new ListNode(9, l2n1);
-        ListNode l2n3 = new ListNode(9, l2n2);
-        ListNode l2n4 = new ListNode(9, l2n3);
-
-        ListNode output = addTwoNumbers.addTwoNumbers(l1n7, l2n4);
-        int[] expect = {8,9,9,9,0,0,0,1};
-        for (int expectVal : expect) {
-            assertThat(output).isNotNull();
-            assertThat(output.val).isEqualTo(expectVal);
-            output = output.next;
-        }
+        ListNode node1 = createLinkedList(new int[]{9,9,9,9,9,9,9});
+        ListNode node2 = createLinkedList(new int[]{9,9,9,9});
+        ListNode expect = createLinkedList(new int[]{8,9,9,9,0,0,0,1});
+        assertThat(addTwoNumbers.addTwoNumbers(node1, node2)).isEqualTo(expect);
     }
 }
